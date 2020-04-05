4c4e63d2a05e40555784a368c85644d6980bd852
Chuckylee <1613786@hcmut.edu.vn>
2020-04-05 19:54:00 +0700

    Complete get RSSI

--- TrainingApp/src/app/home/home.page.ts
+++ TrainingApp/src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
+declare var WifiWizard2: any;
+declare var WifiWizard: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  results = [];
+  info_txt = '';
+  async getNetworks() {
+    this.info_txt = 'loading...';
+    try {
+      let results = await WifiWizard2.scan();
+      this.results = results;
+      this.info_txt = '';
+    } catch (error) {
+      this.info_txt = error;
+    }
+  }
 }
